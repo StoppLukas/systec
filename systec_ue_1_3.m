308090a3d1a64aec84e192073e7dccfcf81f8d96
StoppLukas <lustoppe@htwg-konstanz.de>
2015-10-27 17:19:08 +0100

    init repro and add first matlab file

---
+++ systec_ue_1_3.m
@@ -0,0 +1,27 @@
+x1_beginning = 45;
+x1alt = x1_beginning;
+x2alt = 0;
+x1neu = 0;
+x2neu = 0;
+
+l = 10;
+g = 9.80665;
+tend = 90;
+tstep = 0.001;
+
+vec_x1neu = [];
+vec_x2neu = [];
+vec_t = 0:tstep:tend;
+
+for t = 0:tstep:tend
+    x1neu = x1alt + tstep*x2alt;
+    x2neu = x2alt + tstep*(-g/l*sin(x1alt));
+    vec_x1neu = [vec_x1neu, x1neu];
+    vec_x2neu = [vec_x2neu, x2neu];
+    x1alt = x1neu;
+    x2alt = x2neu;
+end
+
+plot(vec_t, vec_x1neu, vec_t, vec_x2neu)
+xlabel('t');
+ylabel('phi/speed');
