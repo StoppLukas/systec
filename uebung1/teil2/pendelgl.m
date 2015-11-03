9b3a916e39092033e45d4625f3721a52ee21cbb0
dbarelop <dbarelop@gmail.com>
2015-11-03 13:53:46 +0100

    Fixed pendelgl function to be used with ode23

--- uebung1/teil2/pendelgl.m
+++ uebung1/teil2/pendelgl.m
@@ -1,19 +1,9 @@
 function x_dot = pendelgl(t, x)
 	G = 9.8;
 	L = 10;
-	TSTEP = 0.01;
-	TEND = 20;
 
-	vec_x1_new = [];
-	vec_x2_new = [];
-	for t = 0:TSTEP:TEND
-		x1_new = x(1) + TSTEP*x(2);
-		x2_new = x(2) + TSTEP*(-G/L*sin(x(1)));
-		vec_x1_new = [vec_x1_new, x1_new];
-		vec_x2_new = [vec_x2_new, x2_new];
-		x(1) = x1_new;
-		x(2) = x2_new;
-	end
+	x1_new = x(2);
+	x2_new = -G/L*sin(x(1));
 
-	x_dot = [vec_x1_new; vec_x2_new];
+	x_dot = [x1_new; x2_new];
 end
