708800e430a86c99bfa06f0d9b73f438f2a0a31f
Konstantin Wurster <kowurste@htwg-konstanz.de>
2015-11-24 16:06:03 +0100

    3b

---
+++ uebung4/teil3/3b.m
@@ -0,0 +1,20 @@
+sys=tf([0.2],[0.2 1]);
+
+figure(1);
+step(sys);
+
+figure(2);
+bode(sys);
+
+figure(3);
+nyquist(sys);
+
+figure(4);
+impulse(sys);
+
+figure(5);
+%1200period, 1300duration, sampling10
+[a,b] = gensig('square',1200,1300,10);
+lsim(sys,a,b)
+
+dcgain(sys)
