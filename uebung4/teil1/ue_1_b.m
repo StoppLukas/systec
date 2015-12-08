8186449c1e66e17388dffbd516d4ab71d4a0a3d2
dbarelop <dbarelop@gmail.com>
2015-12-08 14:29:34 +0100

    Implemented exercise 4, part 1b without using the function freqz

--- uebung4/teil1/ue_1_b.m
+++ uebung4/teil1/ue_1_b.m
@@ -4,10 +4,20 @@ R = 0.5;
 b = 1;
 a = [C^2*R^2 3*C*R 1];
 
-[g,w] = freqz(b, a, 'whole');
+% Calculation manually
+% ====================
+w = linspace(0, 2*pi);	% vector of angular frequencies
+% Evaluate transfer function and take the amplitude
+g = polyval(b, exp(-1j*w)) ./ polyval(a, exp(-1j*w));
+
+% Calculation using freqz function
+% ================================
+[g_2,w_2] = freqz(b, a, 'whole');
 
 figure
 plot(w/pi, 20*log10(abs(g)))
+hold on
+plot(w_2/pi, 20*log10(abs(g_2)), '--r')
 xlabel('Frequency response (\times\pi rad/sample)');
 ylabel('Magnitude (dB)');
 title('Frequency response');
