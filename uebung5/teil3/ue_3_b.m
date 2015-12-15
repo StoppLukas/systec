93623d91f76324b29b2ced7ad2ad296ae359e469
dbarelop <dbarelop@gmail.com>
2015-12-15 15:41:14 +0100

    Corrected old variable

--- uebung5/teil3/ue_3_b.m
+++ uebung5/teil3/ue_3_b.m
@@ -7,26 +7,26 @@ D = [-10 -1 1 10 -10 -1 1 10 -10 -1 1 10];
 figure
 n = size(W0, 2);
 for k = 1:n
-	H(index) = tf([W0(k)^2], [1 2*D(k)*W0(k) W0(k)^2]);
+	H(k) = tf([W0(k)^2], [1 2*D(k)*W0(k) W0(k)^2]);
 	subplot(3, 4, k);
-	step(H(index));
+	step(H(k));
 	title(sprintf('W0 = %d, D = %d', W0(k), D(k)));
 end
 
 figure
 n = size(W0, 2);
 for k = 1:n
-	H(index) = tf([W0(k)^2], [1 2*D(k)*W0(k) W0(k)^2]);
+	H(k) = tf([W0(k)^2], [1 2*D(k)*W0(k) W0(k)^2]);
 	subplot(3, 4, k);
-	pzmap(H(index));
+	pzmap(H(k));
 	title(sprintf('W0 = %d, D = %d', W0(k), D(k)));
 end
 
 figure
 n = size(W0, 2);
 for k = 1:n
-	H(index) = tf([W0(k)^2], [1 2*D(k)*W0(k) W0(k)^2]);
+	H(k) = tf([W0(k)^2], [1 2*D(k)*W0(k) W0(k)^2]);
 	subplot(3, 4, k);
-	impulse(H(index));
+	impulse(H(k));
 	title(sprintf('W0 = %d, D = %d', W0(k), D(k)));
 end
